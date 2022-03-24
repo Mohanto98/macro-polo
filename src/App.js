13c7501beb4126d33fd9de67562deac87ff6fa50
Subhajyoti Mohanta <subhajyoti.mohanto@gmail.com>
2022-03-24 22:27:03 +0530

    My First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            isToggleOn : false,
+            isHeaderChange : false
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick () {
+        this.setState(state => ({
+            isToggleOn : !state.isToggleOn,
+            isHeaderChange : !state.isHeaderChange
+        }));
+    }
+
+    render () {
+        return (
+            <div className='App'>
+                <h1 id='macro-polo'>{this.state.isHeaderChange ? 'Polo' : "Macro"}</h1>
+                <button id='macro-polo-toggler' onClick={this.handleClick}>{this.state.isToggleOn ? "Macro" : "Polo"}</button>
+            </div>
+        );
+    }
+
 }
 
 export default App;
